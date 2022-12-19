5e5cb22ac240cba602937d55e5750c902e6e164e
cgombauld <clement.gombauld@cpexterne.org>
2022-12-19 09:36:43 +0100

    deleted useless code

--- app/core/backend/articles/entities/articles.ts
+++ app/core/backend/articles/entities/articles.ts
@@ -1,40 +1,11 @@
-import { articleValidation } from "./validation/article-validation";
-import { calcReadingTime } from "./reading-time/calculate-reading-time";
-
-type ArticleProps = {
-  id?: string;
-  title?: string;
-  summary?: string;
-  date?: number;
-  hide?: boolean;
-  content?: Record<string, any>[];
-  lightMode?: boolean;
-  topic?: string | null;
+export type Article = {
+  id: string;
+  title: string;
+  summary: string;
+  date: number;
+  hide: boolean;
+  content: Record<string, any>[];
+  lightMode: boolean;
+  timeToRead: string;
+  topic: string | null;
 };
-
-export const buildArticle = ({
-  id = "",
-  title = "",
-  summary = "",
-  date = 0,
-  hide = false,
-  content = [],
-  lightMode = false,
-  topic = null,
-}: ArticleProps) => {
-  articleValidation({ id, title, content, date });
-
-  return {
-    id,
-    title,
-    summary,
-    date,
-    hide,
-    content,
-    lightMode,
-    timeToRead: calcReadingTime(content),
-    topic,
-  };
-};
-
-export type Article = ReturnType<typeof buildArticle>;
