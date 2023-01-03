cd751efd8465757c3dcd9bef5ad26b455b0046f5
cgombauld <clement.gombauld@cpexterne.org>
2023-01-03 08:22:43 +0100

    added glue code to work with mongo db

---
+++ infrastructure/backend/db/build-articles-repository.ts
@@ -0,0 +1,12 @@
+import clientPromise from "./mongodb";
+import { buildInMemoryArticlesRepository } from "../articles/in-memory-articles-repository";
+import { buildMongoDbArticlesRepository } from "../articles/mongodb-articles-repository";
+
+export const buildArticlesRepository = async () => {
+  if (process.env.NEXT_APP_ARG === "in-memory") {
+    return buildInMemoryArticlesRepository();
+  }
+  const client = await clientPromise;
+  const db = client.db(process.env.DB_NAME);
+  return buildMongoDbArticlesRepository(db);
+};
