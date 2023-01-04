e9657f23df9cde6f8c276c0b4a833258cd2e66cf
cgombauld <clement.gombauld@cpexterne.org>
2023-01-04 09:41:17 +0100

    seo enhanced

--- app/article/[id]/head.tsx
+++ app/article/[id]/head.tsx
@@ -5,6 +5,7 @@ const Head = async ({ params: { id } }: { params: { id: string } }) => {
 
   return (
     <>
+      <meta name="robots" content="index,follow" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#000000" />
       <meta name="description" content={article.summary} />
--- app/head.tsx
+++ app/head.tsx
@@ -1,5 +1,6 @@
 const Head = () => (
   <>
+    <meta name="robots" content="index,follow" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="theme-color" content="#000000" />
     <meta
