9ccc67aa7b01b24bcb27b427b538aeb9565f4a54
cgombauld <clement.gombauld@cpexterne.org>
2023-01-08 13:30:50 +0100

    added cache for mongo db

--- infrastructure/backend/db/build-articles-repository.ts
+++ infrastructure/backend/db/build-articles-repository.ts
@@ -1,4 +1,4 @@
-import clientPromise from "./mongodb";
+import initDb from "./mongodb";
 import { buildInMemoryArticlesRepository } from "../articles/in-memory-articles-repository";
 import { buildMongoDbArticlesRepository } from "../articles/mongodb-articles-repository";
 
@@ -6,6 +6,7 @@ export const buildArticlesRepository = async () => {
   if (process.env.NEXT_APP_ARG === "in-memory") {
     return buildInMemoryArticlesRepository();
   }
+  const clientPromise = initDb();
   const client = await clientPromise;
   const db = client.db(process.env.DB_NAME);
   return buildMongoDbArticlesRepository(db);
