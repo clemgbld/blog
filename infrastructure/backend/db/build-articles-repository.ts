b237c57f779e66a84d1567b6c5c6461def0291ab
cgombauld <clement.gombauld@cpexterne.org>
2023-01-08 13:34:59 +0100

    rollback

--- infrastructure/backend/db/build-articles-repository.ts
+++ infrastructure/backend/db/build-articles-repository.ts
@@ -1,4 +1,4 @@
-import initDb from "./mongodb";
+import clientPromise from "./mongodb";
 import { buildInMemoryArticlesRepository } from "../articles/in-memory-articles-repository";
 import { buildMongoDbArticlesRepository } from "../articles/mongodb-articles-repository";
 
@@ -6,7 +6,6 @@ export const buildArticlesRepository = async () => {
   if (process.env.NEXT_APP_ARG === "in-memory") {
     return buildInMemoryArticlesRepository();
   }
-  const clientPromise = initDb();
   const client = await clientPromise;
   const db = client.db(process.env.DB_NAME);
   return buildMongoDbArticlesRepository(db);
