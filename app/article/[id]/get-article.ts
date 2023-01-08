41e39f6388513dc78838339822c969b6b22742ae
cgombauld <clement.gombauld@cpexterne.org>
2023-01-08 13:38:39 +0100

    delete loading

--- app/article/[id]/get-article.ts
+++ app/article/[id]/get-article.ts
@@ -2,6 +2,8 @@ import { cache } from "react";
 import { retrievePublishedArticle } from "../../../core/backend/articles/use-cases/retrieve-published-article";
 import { buildArticlesRepository } from "../../../infrastructure/backend/db/build-articles-repository";
 
+export const revalidate = 1000;
+
 export const getArticle = cache(async (id: string) => {
   const articlesRepository = await buildArticlesRepository();
   return retrievePublishedArticle({
