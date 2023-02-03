b362490f3109dd04469c073448176368a0fb5f36
cgombauld <clement.gombauld@cpexterne.org>
2023-02-03 21:03:33 +0100

    fixed adaptDataForApp type

--- infrastructure/backend/db/utils/adapt-data.ts
+++ infrastructure/backend/db/utils/adapt-data.ts
@@ -1,9 +1,9 @@
 import { WithId, Document, ObjectId } from "mongodb";
 
-export const adaptDataForApp = (data: any) => {
+export const adaptDataForApp = <T extends { _id?: ObjectId }>(data: T) => {
   if (!data) return undefined;
 
-  const id = data._id.toString();
+  const id = data._id?.toString();
   const adaptedData = { ...data };
   delete adaptedData._id;
   return { id, ...adaptedData };
