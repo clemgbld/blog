83b3b936989fc7615dccc4b4810dbee025383124
cgombauld <clement.gombauld@cpexterne.org>
2023-03-18 16:10:27 +0100

    implemented successfull subscription

--- core/frontend/articles/use-cases/__tests__/subscribe-to-news-letter.test.ts
+++ core/frontend/articles/use-cases/__tests__/subscribe-to-news-letter.test.ts
@@ -4,26 +4,43 @@ type InMemorySubscriptionGatewayProps = {
   spy?: (email: string) => Promise<void>;
 };
 
+type inMemoryNotificationServiceProps = {
+  successSpy?: (message: string) => void;
+  errorSpy?: (message: string) => void;
+};
+
 const buildInMemorySubscriptionGateway = ({
   spy,
 }: InMemorySubscriptionGatewayProps) => ({
   subscribe: spy ? spy : async (email: string) => Promise.resolve(),
 });
 
+const buildInMemoryNotificationService = ({
+  successSpy,
+  errorSpy,
+}: inMemoryNotificationServiceProps) => ({
+  success: successSpy ? successSpy : (message: string) => {},
+  error: errorSpy ? errorSpy : (message: string) => {},
+});
+
 describe("subscribe a new user to the blog news letter", () => {
   describe("user email handling", () => {
     it("should have an empty email initially", () => {
       const subscriptionGateway = buildInMemorySubscriptionGateway({});
+      const notificationService = buildInMemoryNotificationService({});
       const subscriptionStore = createSubscriptionStore({
         subscriptionGateway,
+        notificationService,
       });
       expect(subscriptionStore.getState().email).toBe("");
     });
 
     it("should update the user email", () => {
       const subscriptionGateway = buildInMemorySubscriptionGateway({});
+      const notificationService = buildInMemoryNotificationService({});
       const subscriptionStore = createSubscriptionStore({
         subscriptionGateway,
+        notificationService,
       });
       subscriptionStore.getState().updateUserEmail("example@hotmail.fr");
       expect(subscriptionStore.getState().email).toBe("example@hotmail.fr");
@@ -33,12 +50,28 @@ describe("subscribe a new user to the blog news letter", () => {
   describe("subscription handling", () => {
     it("should sucessfully subscriber the user to the blog news letter", async () => {
       const subscribeSpy = jest.fn().mockResolvedValue(undefined);
+      const successNotificationSpy = jest.fn();
+
+      const notificationService = buildInMemoryNotificationService({
+        successSpy: successNotificationSpy,
+      });
+
       const subscriptionGateway = buildInMemorySubscriptionGateway({
         spy: subscribeSpy,
       });
       const subscriptionStore = createSubscriptionStore({
         subscriptionGateway,
+        notificationService,
       });
+
+      subscriptionStore.getState().updateUserEmail("example@hotmail.fr");
+      await subscriptionStore.getState().subscribeBlogReader();
+      expect(subscribeSpy).toHaveBeenCalledWith("example@hotmail.fr");
+      expect(successNotificationSpy).toHaveBeenCalledWith(
+        "Successfully subscribed to the news letter"
+      );
+      expect(subscriptionStore.getState().email).toBe("");
+      expect(subscriptionStore.getState().isLoading).toBe(false);
     });
   });
 });
--- core/frontend/articles/use-cases/subscribe-to-news-letter.ts
+++ core/frontend/articles/use-cases/subscribe-to-news-letter.ts
@@ -4,19 +4,34 @@ type SubscriptionGateway = {
   subscribe: (email: string) => Promise<void>;
 };
 
+type NotificationService = {
+  success: (message: string) => void;
+  error: (message: string) => void;
+};
+
 type Subscription = {
   email: string;
+  isLoading: false;
   updateUserEmail: (email: string) => void;
+  subscribeBlogReader: () => Promise<void>;
 };
 
 type SubscriptionStoreDependency = {
   subscriptionGateway: SubscriptionGateway;
+  notificationService: NotificationService;
 };
 
-export const createSubscriptionStore = (
-  dependency: SubscriptionStoreDependency
-) =>
-  create<Subscription>((set) => ({
+export const createSubscriptionStore = ({
+  subscriptionGateway,
+  notificationService,
+}: SubscriptionStoreDependency) =>
+  create<Subscription>((set, getState) => ({
     email: "",
+    isLoading: false,
     updateUserEmail: (email: string) => set({ email }),
+    subscribeBlogReader: async () => {
+      await subscriptionGateway.subscribe(getState().email);
+      notificationService.success("Successfully subscribed to the news letter");
+      getState().updateUserEmail("");
+    },
   }));
