dad11ae9881d8d745a4e6331dd2d5f5c93e3eb5d
cgombauld <clement.gombauld@cpexterne.org>
2023-04-01 19:36:39 +0200

    fix build and integration tests

--- infrastructure/backend/articles/__tests__/mongodb-articles-repository.test.ts
+++ infrastructure/backend/articles/__tests__/mongodb-articles-repository.test.ts
@@ -1,104 +1,103 @@
-// /**
-//  * @jest-environment node
-//  */
+/**
+ * @jest-environment node
+ */
 
-// import { MongoClient, Db } from "mongodb";
-// import { MongoMemoryServer } from "mongodb-memory-server";
-// import { generateId } from "../../id-generator/generate-id";
-// import { initMemoryDb } from "../../db/db";
-// import { buildMongoDbArticlesRepository } from "../mongodb-articles-repository";
-// import {
-//   fakeArticle1WithContentStringify,
-//   fakeArticle2WithContentStringify,
-// } from "../../../../core/backend/articles/fixtures/articles-fixtures";
+import { MongoClient, Db } from "mongodb";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import { generateId } from "../../id-generator/generate-id";
+import { initMemoryDb } from "../../db/db";
+import { buildMongoDbArticlesRepository } from "../mongodb-articles-repository";
+import {
+  fakeArticle1WithContentStringify,
+  fakeArticle2WithContentStringify,
+} from "../../../../core/backend/articles/fixtures/articles-fixtures";
 
-// import { ArticleWithStringifyContent } from "../../../../core/backend/articles/repositories/articles-repository";
-// import { adaptIdForMongoDB } from "../../db/utils/adapt-data";
+import { ArticleWithStringifyContent } from "../../../../core/backend/articles/repositories/articles-repository";
+import { adaptIdForMongoDB } from "../../db/utils/adapt-data";
 
-// let db: Db;
-// let connection: MongoClient;
-// let mongoServer: MongoMemoryServer;
-// let articlesRepository: ReturnType<typeof buildMongoDbArticlesRepository>;
+let db: Db;
+let connection: MongoClient;
+let mongoServer: MongoMemoryServer;
+let articlesRepository: ReturnType<typeof buildMongoDbArticlesRepository>;
 
-// export const adaptDataForMongoDb = (
-//   data: Partial<ArticleWithStringifyContent>
-// ) => {
-//   const id = data.id;
-//   const adaptedData = { ...data };
-//   delete adaptedData.id;
-//   return { _id: adaptIdForMongoDB(id || ""), ...adaptedData };
-// };
+export const adaptDataForMongoDb = (
+  data: Partial<ArticleWithStringifyContent>
+) => {
+  const id = data.id;
+  const adaptedData = { ...data };
+  delete adaptedData.id;
+  return { _id: adaptIdForMongoDB(id || ""), ...adaptedData };
+};
 
-// describe("mongodb articles repository", () => {
-//   beforeAll(async () => {
-//     const dbData = await initMemoryDb();
-//     db = dbData.db;
-//     connection = dbData.connection;
-//     mongoServer = dbData.mongoServer;
-//   });
+describe("mongodb articles repository", () => {
+  beforeAll(async () => {
+    const dbData = await initMemoryDb();
+    db = dbData.db;
+    connection = dbData.connection;
+    mongoServer = dbData.mongoServer;
+  });
 
-//   beforeEach(() => {
-//     articlesRepository = buildMongoDbArticlesRepository(db);
-//   });
+  beforeEach(() => {
+    articlesRepository = buildMongoDbArticlesRepository(db);
+  });
 
-//   afterEach(async () => {
-//     await db.collection("articles").deleteMany({});
-//   });
+  afterEach(async () => {
+    await db.collection("articles").deleteMany({});
+  });
 
-//   afterAll(async () => {
-//     await mongoServer.stop();
-//     await connection.close();
-//   });
+  afterAll(async () => {
+    await mongoServer.stop();
+    await connection.close();
+  });
 
-//   describe("mongodb articles repository", () => {
-//     describe("get all published articles", () => {
-//       it("should get all the published articles", async () => {
-//         const hiddenFakeArticle = {
-//           ...fakeArticle2WithContentStringify,
-//           hide: true,
-//           id: generateId(),
-//         };
+  describe("mongodb articles repository", () => {
+    describe("get all published articles", () => {
+      it("should get all the published articles", async () => {
+        const hiddenFakeArticle = {
+          ...fakeArticle2WithContentStringify,
+          hide: true,
+          id: generateId(),
+        };
 
-//         const publishedArticle = {
-//           ...fakeArticle1WithContentStringify,
-//           id: generateId(),
-//         };
+        const publishedArticle = {
+          ...fakeArticle1WithContentStringify,
+          id: generateId(),
+        };
 
-//         await db
-//           .collection("articles")
-//           .insertMany([
-//             adaptDataForMongoDb(hiddenFakeArticle),
-//             adaptDataForMongoDb(publishedArticle),
-//           ]);
+        await db
+          .collection("articles")
+          .insertMany([
+            adaptDataForMongoDb(hiddenFakeArticle),
+            adaptDataForMongoDb(publishedArticle),
+          ]);
 
-//         expect(await articlesRepository.allArticlesPublished()).toEqual([
-//           publishedArticle,
-//         ]);
-//       });
-//     });
+        expect(await articlesRepository.allArticlesPublished()).toEqual([
+          publishedArticle,
+        ]);
+      });
+    });
 
-//     describe("get published article", () => {
-//       it("should get the expected article", async () => {
-//         const id = generateId();
-//         const publishedArticle = {
-//           ...fakeArticle1WithContentStringify,
-//           id: id,
-//         };
-//         await db
-//           .collection("articles")
-//           .insertOne(adaptDataForMongoDb(publishedArticle));
-//         expect(await articlesRepository.getPublishedArticle(id)).toEqual(
-//           publishedArticle
-//         );
-//       });
+    describe("get published article", () => {
+      it("should get the expected article", async () => {
+        const id = generateId();
+        const publishedArticle = {
+          ...fakeArticle1WithContentStringify,
+          id: id,
+        };
+        await db
+          .collection("articles")
+          .insertOne(adaptDataForMongoDb(publishedArticle));
+        expect(await articlesRepository.getPublishedArticle(id)).toEqual(
+          publishedArticle
+        );
+      });
 
-//       it("should be undefined when the article is not published", async () => {
-//         const id = generateId();
-//         expect(await articlesRepository.getPublishedArticle(id)).toEqual(
-//           undefined
-//         );
-//       });
-//     });
-//   });
-// });
-export {};
+      it("should be undefined when the article is not published", async () => {
+        const id = generateId();
+        expect(await articlesRepository.getPublishedArticle(id)).toEqual(
+          undefined
+        );
+      });
+    });
+  });
+});
